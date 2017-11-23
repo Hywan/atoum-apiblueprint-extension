29c6a3e7b18a2779f0b53bc5631c03056ddcc2e6
Ivan Enderlin <ivan.enderlin@hoa-project.net>
2017-11-23 14:57:31 +0100

    test(system) Find a workaround for an atoum's bug.
    Please, see https://github.com/atoum/atoum/pull/781 to learn more.

--- test/system/FullRun.php
+++ test/system/FullRun.php
@@ -23,9 +23,13 @@ class FullRun extends test
 
     public function test_from_apib_files_to_atoum_tests()
     {
-        $files     = new \FilesystemIterator(dirname(__DIR__) . '/fixtures/system/fullRun');
-        $parser    = new Parser();
-        $target    = new Target();
+        $files  = new \FilesystemIterator(__DIR__ . '/ApibToTestCase/');
+        $parser = new Parser();
+        $target = new Target();
+
+        $resource  = (new \ReflectionClass(file::class))->getProperty('resource');
+        $resource->setAccessible(true);
+
         $uri       = stream_get_meta_data(tmpfile())['uri'];
         $collector = new file($uri);
 
@@ -45,7 +49,9 @@ class FullRun extends test
                 ->string($output)
                     ->isEqualTo(file_get_contents($uri));
 
-            $collector->clear();
+            $collectorFileDescriptor = $resource->getValue($collector);
+            ftruncate($collectorFileDescriptor, 0);
+            rewind($collectorFileDescriptor);
         }
     }
 }
