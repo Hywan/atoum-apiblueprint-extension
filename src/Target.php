d6cf2e5661f57e28a03981ed22395a8c559d71a1
Ivan Enderlin <ivan.enderlin@hoa-project.net>
2017-11-24 13:52:32 +0100

    feat(target) Compile resources and (empty) actions.

--- src/Target.php
+++ src/Target.php
@@ -5,11 +5,12 @@ declare(strict_types=1);
 namespace atoum\apiblueprint;
 
 use Hoa\Ustring;
+use atoum\apiblueprint\IntermediateRepresentation as IR;
 use mageekguy\atoum\writers\file;
 
 class Target
 {
-    public function compile(IntermediateRepresentation\Document $document, file $outputFile)
+    public function compile(IR\Document $document, file $outputFile)
     {
         $outputFile->write('namespace atoum\apiblueprint\generated;' . "\n\n");
 
@@ -18,7 +19,7 @@ class Target
         if (empty($testSuiteName)) {
             $testSuiteName = 'Unknown' . sha1(serialize($document));
         } else {
-            $testSuiteName = (new UString($testSuiteName))->toAscii()->replace('/[^a-zA-Z0-9_\x80-\xff]/', '__');
+            $testSuiteName = $this->stringToPHPIdentifier($testSuiteName, false);
         }
 
         $outputFile->write(
@@ -31,6 +32,72 @@ class Target
             '    }'
         );
 
+        foreach ($document->resources as $resource)
+        {
+            $this->compileResource($resource, $outputFile);
+        }
+
         $outputFile->write("\n" . '}');
+    }
+
+    public function compileResource(IR\Resource $resource, file $outputFile)
+    {
+        static $_unknownResourceId = 0;
+
+        $resourceName = $resource->name;
+
+        if (empty($resourceName)) {
+            $resourceName = 'test resource unknonwn' . $_unknownResourceId++;
+        } else {
+            $resourceName = 'test resource ' . $this->stringToPHPIdentifier($resourceName);
+        }
+
+        if (empty($resource->actions)) {
+            $outputFile->write(
+                "\n\n" .
+                '    public function ' . $resourceName . '()' . "\n" .
+                '    {' . "\n" .
+                '        $this->skip(\'No action for the resource `' . $resource->name . '`.\');' . "\n" .
+                '    }'
+            );
+
+            return;
+        }
+
+        foreach ($resource->actions as $i => $action) {
+            $actionName = $action->name;
+
+            if (empty($actionName)) {
+                $actionName = 'action unknown' . $i;
+            } else {
+                $actionName = 'action ' . $this->stringToPHPIdentifier($actionName);
+            }
+
+            $this->compileAction($action, $actionName, $resource, $resourceName, $outputFile);
+        }
+    }
+
+    public function compileAction(IR\Action $action, string $actionName, IR\Resource $resource, string $resourceName, file $outputFile)
+    {
+        if (empty($action->messages)) {
+            $outputFile->write(
+                "\n\n" .
+                '    public function ' . $resourceName . ' ' . $actionName . '()' . "\n" .
+                '    {' . "\n" .
+                '        $this->skip(\'Action `' .$action->name . '` for the resource `' . $resource->name . '` has no message.\');' . "\n" .
+                '    }'
+            );
+        }
+    }
+
+    public function stringToPHPIdentifier(string $string, bool $toLowerCase = true): string
+    {
+        $identifier = (new UString($string))->toAscii()->replace('/[^a-zA-Z0-9_\x80-\xff]/', ' ');
+
+        if (true === $toLowerCase) {
+            $identifier->toLowerCase();
+        }
+
+        return trim((string) $identifier, ' ');
     }
 }
